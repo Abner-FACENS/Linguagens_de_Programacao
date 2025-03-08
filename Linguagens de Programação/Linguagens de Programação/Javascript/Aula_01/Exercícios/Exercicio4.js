e4f1832bfcd785bed369c83f6571ce5dd3e0c326
Abner-FACENS <247832@facens.br>
2025-03-07 22:35:30 -0300

    Delete Linguagens de Programação/Linguagens de Programação directory

--- Linguagens de Programação/Linguagens de Programação/Javascript/Aula_01/Exercícios/Exercicio4.js
+++
@@ -1,8 +0,0 @@
-//Faça um programa que recebe o nome de uma variável e mostre o
-//<<primeiro>>.<<último nome>>@facens.br
-
-let nome = "Abner Matheus Marques Rodrigues";
-
-let array = nome.split(" ");
-
-console.log(array[0] + "." + array[array.length - 1] + "@facens.br");
--- Linguagens de Programação/Linguagens de Programação/Javascript/Aula_02/Exercícios/exercicio4.js
+++
@@ -1,3 +0,0 @@
-let dividir = (int) => int%3 == 0;
-
-console.log("A constatação do seu número ser dividido por três é: ", dividir(6))
